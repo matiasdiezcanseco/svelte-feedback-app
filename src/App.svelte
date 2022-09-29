889a9608767716ab8f46061d118051ac9070b559
Matias Diez-Canseco <matiasdiezcanseco@gmail.com>
2022-09-28 23:12:51 -0500

    Added router

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+  import { Router, Route } from 'svelte-navigator'
+  import Card from './components/Card.svelte'
   import FeedbackForm from './components/FeedbackForm.svelte'
   import FeedbackList from './components/FeedbackList.svelte'
   import FeedbackStats from './components/FeedbackStats.svelte'
+  import NavBar from './components/NavBar.svelte'
 </script>
 
-<main class="container">
-  <FeedbackForm />
-  <FeedbackStats />
-  <FeedbackList />
-</main>
+<Router>
+  <main class="container">
+    <NavBar />
+    <Route path="/">
+      <FeedbackForm />
+      <FeedbackStats />
+      <FeedbackList />
+    </Route>
+
+    <Route path="/about">
+      <Card>About Us</Card>
+    </Route>
+  </main>
+</Router>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Link } from 'svelte-navigator'
+</script>
+
+<nav>
+  <Link to="/">Rate Us</Link>
+  <Link to="about">About</Link>
+</nav>
